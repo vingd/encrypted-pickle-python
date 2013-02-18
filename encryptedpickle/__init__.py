7656233598e02e65971f69e11849a0f288b2b2a5
Andjelko Horvat <comel@vingd.com>
2013-02-18 14:23:06 +0100

    Version 0.1.4.

--- encryptedpickle/__init__.py
+++ encryptedpickle/__init__.py
@@ -1,7 +1,7 @@
 '''Class for pickling and encrypting data'''
 
 __title__ = 'EncryptedPickle'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __author__ = 'Vingd, Inc.'
 __author_email__ = 'developers@vingd.com'
 __url__ = 'https://github.com/vingd/encrypted-pickle-python'
